c0667e185ea9744317ba918f7a98efa72c5d6890
hakjaeson <hakjae.son@gmail.com>
2023-11-13 11:50:43 +0900

    feat : test

---
+++ README.md
@@ -0,0 +1,16 @@
+#인터파크 백엔드 프로젝트
+
+- 인터파크 백엔드 스터디
+- [결과물 보기](https://github.com/kimusauce/interpark-client-clone)
+- CloudType 배포
+
+## 프로젝트 설명
+
+### 적용기술
+
+- Node.js
+- express
+
+### 기타사항
+
+- 프로젝트 생성
---
+++ index.js
@@ -0,0 +1,149 @@
+// const express = require("express") ;  옛날 즉 commonjs 방식
+import express from "express"; // 현대 방식 module 방식
+import cors from "cors";
+// 도움말 및 기능 테스트 Swagger
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import { options } from "./swagger/config.js";
+
+const app = express();
+const port = 4000; // 서버에 접속시 포트번호
+// cors 처리(웹브라우저로 접속시 보안관련 처리)
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// json 데이터를 사용하겠다고 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// swagger 설정
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// REST API 요청 처리
+// 첫페이지
+app.get("/", function (req, res) {
+  res.send("인터파크 API");
+});
+// 게시판 API (백엔드 호출 함수)
+// get 은 프론트에서 자료 요청
+// localhost:4000/board : 게시판 자료를 요청.
+app.get("/board", (req, res) => {
+  console.log("GET", req);
+  // DB 에서 조건을 보고 결과를 {} 만들어서 [] 담아서준다.
+  // MongoDB, MaraiDB(MySql)
+  const result = [
+    {
+      number: 1,
+      writer: "철수",
+      title: "제목입니다.",
+      contents: "내용입니다.",
+    },
+    {
+      number: 2,
+      writer: "영희",
+      title: "영희입니다.",
+      contents: "내용입니다.",
+    },
+    {
+      number: 3,
+      writer: "훈희",
+      title: "훈희입니다.",
+      contents: "내용입니다.",
+    },
+  ];
+  res.send(result);
+});
+
+// post 는 프론트에서 백엔드로 자료 전송
+// localhost:4000/board : 게시판 자료를 추가한다.
+// axios.post("/board", {자료})
+app.post("/board", (req, res) => {
+  // console.log("POST", req);
+  console.log("BODY 프론트가 보낸 데이터 ", req.body);
+  // req.body 를 바탕으로 DB 에 내용 추가
+  res.send("게시물 추가에 성공하였습니다.");
+});
+
+// 인터파크 API (백엔드 호출 함수)
+// visual 영역에 출력할 자료 요청
+// localhost:4000/visual
+app.get("/visual", (req, res) => {
+  const result = {
+    total: 6,
+    visual_1: {
+      file: "images/v1.png",
+      url: "a.html",
+    },
+    visual_2: {
+      file: "images/v2.jpg",
+      url: "b.html",
+    },
+    visual_3: {
+      file: "images/v3.jpg",
+      url: "c.html",
+    },
+    visual_4: {
+      file: "images/v4.jpg",
+      url: "d.html",
+    },
+    visual_5: {
+      file: "images/v5.jpg",
+      url: "e.html",
+    },
+    visual_6: {
+      file: "images/v6.png",
+      url: "f.html",
+    },
+  };
+  res.send(result);
+});
+// recommend 영역에 출력할 자료 요청
+// localhost:4000/visual
+app.get("/recommend", (req, res) => {
+  const result = {
+    total: 7,
+    recommend_1: {
+      file: "images/shoes.jpeg",
+      url: "a.html",
+      discount: "24",
+      price: "7,500",
+      desc: "[스포츠파크]  노스페이스 겨울 구스다운 패딩 슬립온 NS93M50",
+    },
+    recommend_2: {
+      file: "images/shoes.jpeg",
+      url: "b.html",
+      discount: 0,
+      price: "7,500",
+      desc: "[스포츠파크]  노스페이스 겨울 구스다운 패딩 슬립온 NS93M50",
+    },
+    recommend_3: {
+      file: "images/shoes.jpeg",
+      url: "c.html",
+    },
+    recommend_4: {
+      file: "images/shoes.jpeg",
+      url: "d.html",
+    },
+    recommend_5: {
+      file: "images/shoes.jpeg",
+      url: "e.html",
+    },
+    recommend_6: {
+      file: "images/shoes.jpeg",
+      url: "f.html",
+    },
+    recommend_7: {
+      url: "go.html",
+    },
+  };
+  res.send(result);
+});
+
+// 서버에서 Request 요청대기
+app.listen(port, () => {
+  console.log(`현재 웹서버가 ${port} 로 접속하였습니다.`);
+});
